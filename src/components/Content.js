56317f8a7d05fdc08165ae789f16ee8bb2fb7382
aditya allam <aditya@codebrunch.dev>
2024-04-05 17:17:41 +0530

    implementing see top restaurants

--- src/components/Content.js
+++ src/components/Content.js
@@ -2,15 +2,27 @@ import Search from './Search.js';
 import Header from './Header.js';
 import Footer from './Footer.js';
 import RestaurantCard from './RestaurantCard.js';
-import {restaurants} from '../utils/constants.js';
+import { restaurants } from '../utils/constants.js';
+import { useState } from 'react';
 
 const Content = () => {
+    const [restaurantsList, setRestaurantsList] = useState(restaurants);
     return (
-        <div id="content">  
+        <div id="content">
             <div className='content-container'>
-                {Header()} 
-                {Search()}  
-                {restaurants.map((restaurant)=>{
+                {Header()}
+                {Search()}
+                <div className='filter-container'>
+                    <button onClick={() => {
+                        let filteredList = restaurantsList.filter((restaurant) => {
+                            if (restaurant.rating > 4.8) {
+                                return true;
+                            }
+                        });
+                        setRestaurantsList(filteredList);
+                    }}>See Top Restaurants</button>
+                </div>
+                {restaurantsList.map((restaurant) => {
                     return (<RestaurantCard key={restaurant.id} restaurant={restaurant} />);
                 })}
                 {Footer()}
