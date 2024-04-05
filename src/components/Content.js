333fd1c6840b45ef7fc01a0fa5aadf8b35f24b68
aditya allam <aditya@codebrunch.dev>
2024-04-05 20:53:04 +0530

    Added filter functionality and see all restaurants functionality

--- src/components/Content.js
+++ src/components/Content.js
@@ -11,16 +11,21 @@ const Content = () => {
         <div id="content">
             <div className='content-container'>
                 {Header()}
-                {Search()}
+                <Search setResList={setRestaurantsList} restaurants={restaurants}/>
                 <div className='filter-container'>
                     <button onClick={() => {
-                        let filteredList = restaurantsList.filter((restaurant) => {
+                        let filteredList = restaurants.filter((restaurant) => {
                             if (restaurant.rating > 4.8) {
                                 return true;
+                            }else{
+                                return false;
                             }
                         });
                         setRestaurantsList(filteredList);
                     }}>See Top Restaurants</button>
+                    <button onClick={() => {
+                        setRestaurantsList(restaurants);
+                    }}>See All Restaurants</button>
                 </div>
                 {restaurantsList.map((restaurant) => {
                     return (<RestaurantCard key={restaurant.id} restaurant={restaurant} />);
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,10 +1,27 @@
 import searchIcon from '../images/magnifying-glass.png';
-
-const search = <a className='searchBtn'><img src = {searchIcon} width = {'80%'} height={'30'}/></a>;
-const searchInput = (<input type='text' placeholder='Search For Restaurants' className='searchInp'/>);
-const Search = () => 
-(<div className='search-container'>
-{searchInput}{search}
-</div>);
-
+import {useState} from 'react';
+const Search = (props) => {
+    const { setResList, restaurants } = props;
+    const [filterStr, setFilterStr] = useState('');
+    return (<div className='search-container'>
+        <input type='text' value = {filterStr} onChange = {(event)=>{
+            setFilterStr(event.target.value);
+        }} placeholder='Search For Restaurants' className='searchInp' />
+        <a onClick={()=>{
+            let filteredList;
+            if(filterStr.trim() === ''){
+                setResList(restaurants);
+            }else{
+                filteredList = restaurants.filter((res)=>{
+                    if((res.name.toLowerCase()).search(filterStr.toLowerCase()) !== -1){
+                        return true;
+                    }else{
+                        return false;
+                    }
+                });
+                setResList(filteredList);
+            }
+        }} className='searchBtn'><img src={searchIcon} width={'80%'} height={'30'} /></a>
+    </div>);
+}
 export default Search;
