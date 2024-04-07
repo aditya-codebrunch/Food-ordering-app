08f5cfca49bfaad60951ce40e6cda69ca2905615
aditya allam <aditya@codebrunch.dev>
2024-04-07 14:57:28 +0530

    Shimmer UI

--- src/components/Content.js
+++ src/components/Content.js
@@ -2,31 +2,32 @@ import Search from './Search.js';
 import Header from './Header.js';
 import Footer from './Footer.js';
 import RestaurantCard from './RestaurantCard.js';
+import Shimmer from './Shimmer.js';
 import { useState, useEffect } from 'react';
 import { FETCH_MOCK_DATA_URL } from '../utils/constants.js'
 
 let restaurants = [];
 const Content = () => {
     const [restaurantsList, setRestaurantsList] = useState([]);
-    useEffect(()=>{
-        fetch(FETCH_MOCK_DATA_URL).then((response)=>{
+    useEffect(() => {
+        fetch(FETCH_MOCK_DATA_URL).then((response) => {
             return response.json();
-        }).then((data)=>{
+        }).then((data) => {
             restaurants = data;
             setRestaurantsList(restaurants);
         })
-    },[]);
+    }, []);
     return (
         <div id="content">
             <div className='content-container'>
                 {Header()}
-                <Search setResList={setRestaurantsList} restaurants={restaurants}/>
+                <Search setResList={setRestaurantsList} restaurants={restaurants} />
                 <div className='filter-container'>
                     <button onClick={() => {
                         let filteredList = restaurants.filter((restaurant) => {
                             if (restaurant.rating > 4.8) {
                                 return true;
-                            }else{
+                            } else {
                                 return false;
                             }
                         });
@@ -36,9 +37,12 @@ const Content = () => {
                         setRestaurantsList(restaurants);
                     }}>See All Restaurants</button>
                 </div>
-                {restaurantsList.map((restaurant) => {
+                {
+                  restaurantsList.length?
+                   (restaurantsList.map((restaurant) => {
                     return (<RestaurantCard key={restaurant.id} restaurant={restaurant} />);
-                })}
+                    })):(<Shimmer/>)
+                }
                 {Footer()}
             </div>
         </div>
